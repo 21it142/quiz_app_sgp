ae3e7fe6266acb1be44f0572571e996e615e34ad
Gaming buddy <97278175+21it142@users.noreply.github.com>
2023-03-22 22:54:40 +0530

    Add files via upload

---
+++ quiz app/end.js
@@ -0,0 +1,31 @@
+const username = document.getElementById('username');
+const saveScoreBtn = document.getElementById('saveScoreBtn');
+const finalScore=document.getElementById('finalscore');
+const mostRecentScore=localStorage.getItem('mostRecentScore');
+const highScores = JSON.parse(localStorage.getItem("highScores"))||[];
+const MAX_HIGH_SCORES=10;
+finalScore.innerText=mostRecentScore;
+
+username.addEventListener('keyup', () => {
+    saveScoreBtn.disabled = !username.value;
+});
+
+saveHighScore = (e) => {
+    e.preventDefault();
+
+
+    const score={
+        score: mostRecentScore,
+        name: username.value,
+        };
+        highScores.push(score);
+        highScores.sort((a,b)=>b.score-a.score);
+        highScores.splice(10);
+
+        localStorage.setItem('highScores', JSON.stringify(highScores));
+        window.location.assign('/');
+
+};
+
+
+
